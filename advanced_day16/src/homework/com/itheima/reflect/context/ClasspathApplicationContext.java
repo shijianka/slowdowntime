8189963cd4151901f4f1534a09bc7d95bb30603c
shijianka <shijianka@foxmail.com>
2022-01-08 14:05:18 +0800

    解决：  third:若对象集合中不存在对应的对象，那么重新创建并添加到集合中  --处理了一些因外部引入导致的路径错误问题

--- advanced_day16/src/homework/com/itheima/reflect/context/ClasspathApplicationContext.java
+++ advanced_day16/src/homework/com/itheima/reflect/context/ClasspathApplicationContext.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ClasspathApplicationContext implements ApplicationContext {
 
     // 定义一个容器存储系统中可能使用到的对象
-    private static ConcurrentHashMap<String , Object> beans = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, Object> beans = new ConcurrentHashMap<>();
 
     // 定义构造方法
     public ClasspathApplicationContext(String fileName) {
@@ -30,15 +30,15 @@ public class ClasspathApplicationContext implements ApplicationContext {
 
         Set<Map.Entry<Object, Object>> entries = properties.entrySet();
         for (Map.Entry<Object, Object> entry : entries) {
-            Class clazz=null;
+            Class clazz = null;
             try {
                 clazz = Class.forName((String) entry.getValue());
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
 
-            Object o=null;
-            if(clazz!=null) {
+            Object o = null;
+            if (clazz != null) {
                 Constructor constructor = null;
                 try {
                     constructor = clazz.getConstructor();
@@ -56,7 +56,7 @@ public class ClasspathApplicationContext implements ApplicationContext {
                     e.printStackTrace();
                 }
             }
-            if(o!=null) {
+            if (o != null) {
                 beans.put((String) entry.getValue(), o);
             }
         }
@@ -68,12 +68,31 @@ public class ClasspathApplicationContext implements ApplicationContext {
 
         // 补全代码
         String name = clazz.getName();
-        T t1 = (T)beans.get(name);
-        if(t1!=null){
-            return  t1;
+        T t1 = (T) beans.get(name);
+        if (t1 != null) {
+            return t1;
         }
-
-        return null;
+        //若beans集合中没有，则创建对象
+        Constructor<T> declaredConstructor = null;
+        try {
+            declaredConstructor = clazz.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        T t2 = null;
+        if (declaredConstructor != null) {
+            try {
+                t2 = declaredConstructor.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        beans.put(name,t2);
+        return t2;
     }
 
 }
--- advanced_day16/src/homework/com/itheima/reflect/entry/Entry.java
+++ advanced_day16/src/homework/com/itheima/reflect/entry/Entry.java
@@ -1,16 +1,16 @@
 package homework.com.itheima.reflect.entry;
 
-import com.itheima.reflect.context.ApplicationContext;
-import com.itheima.reflect.context.ClasspathApplicationContext;
-import com.itheima.reflect.service.OrderService;
-import com.itheima.reflect.service.UserService;
+import homework.com.itheima.reflect.context.ApplicationContext;
+import homework.com.itheima.reflect.context.ClasspathApplicationContext;
+import homework.com.itheima.reflect.service.OrderService;
+import homework.com.itheima.reflect.service.UserService;
 
 public class Entry {
 
     public static void main(String[] args) {
 
         // 创建ClasspathApplicationContext对象
-        ApplicationContext applicationContext = new ClasspathApplicationContext("applicationContext.properties") ;
+        ApplicationContext applicationContext = new ClasspathApplicationContext("homework\\applicationContext.properties") ;
 
         // 从applicationContext对象获取userService对象，并进行使用
         UserService userService = applicationContext.getBean(UserService.class);
