634ab3a1c1f806e7a02ace84ccbc06bd74a179c3
shijianka <shijianka@foxmail.com>
2022-01-09 18:41:41 +0800

    解决：volatile 在使用的共享变量生声明处加volatile

--- advanced_day14/src/homework/双色球/Enter.java
+++ advanced_day14/src/homework/双色球/Enter.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class Enter extends Thread {
     private ArrayBlockingQueue<Personnel> door;
     private ArrayList<Personnel> list;//门外员工们的集合
-    private boolean[] lock;//表示阻塞队列是否有元素
+    private volatile boolean[] lock;//表示阻塞队列是否有元素
 
     public Enter(ArrayBlockingQueue<Personnel> door, ArrayList<Personnel> list, boolean[] lock) {
         this.door = door;
@@ -22,20 +22,26 @@ public class Enter extends Thread {
         //声明待通过的员工对象
         Personnel personnel;
         //list表示门外的员工集合，若为空则推出循环
-        while (!list.isEmpty()) {
+        while (true) {
             //若门内有人（阻塞队列里面的元素未被取出），则continue，再次判断会场外是否还有人（list是否为空）
             if(lock[0]){
-                try {
+               /* try {
                     sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
+                }*/
+
                 continue;
             }
             //让门外的一个人进入门内
             synchronized (list) {
-                personnel = list.get(list.size() - 1);
-                list.remove(personnel);
+                if (list.size()>0){
+                    personnel = list.get(list.size() - 1);
+                    list.remove(personnel);
+                }else{
+                    break;
+                }
+
             }
             try {
                 //先将表示阻塞队列是否有元素的变量复制，让阻塞队列的take()进入等待状态
@@ -47,6 +53,7 @@ public class Enter extends Thread {
             }
 
         }
+        System.out.println("list为空，结束了...");
     }
 
 }
